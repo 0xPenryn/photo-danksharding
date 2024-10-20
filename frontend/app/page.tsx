f1460b99a4dc0549f2c21dddcf84065e1966aba6
0xPenryn <ian.dilick@toolsforhumanity.com>
2024-10-20 06:41:10 -0700

    wip frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -20,21 +20,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24 gap-4 justify-center text-center">
-      {signerStatus.isInitializing ? (
-        <>Loading...</>
-      ) : user ? (
-        <div className="flex flex-col gap-2 p-2">
-          <p className="text-xl font-bold">Success!</p>
-          Logged in as {user.email ?? "anon"}.
-          <button className="btn btn-primary mt-6" onClick={() => logout()}>
-            Log out
-          </button>
-        </div>
-      ) : (
-        <button className="btn btn-primary" onClick={openAuthModal}>
-          Login
-        </button>
-      )}
+      {/* TODO: display feed from list in redis kv, pull images from blobs via https://api.blobscan.com/blobs/{id}/data */} 
     </main>
   );
 }
